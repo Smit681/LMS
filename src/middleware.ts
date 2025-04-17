213cee80f995272674f2e8d82e6beb0a469de9ad
Smit Shah <smitshah681@gmail.com>
2025-04-17 09:47:17 -0400

    Added security for the app in  middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,29 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api(.*)",
+  "/courses/:courseId/lessons/:lessonId",
+  "/products(.*)",
+]);
+
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isAdminRoute(req)) {
+    const user = await auth.protect();
+    if (user.sessionClaims.role !== "admin") {
+      return new NextResponse(null, { status: 404 });
+    }
+  }
+
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
