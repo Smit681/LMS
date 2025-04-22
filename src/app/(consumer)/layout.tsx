26a1ae1bfac77389388e11432f6105f8af5ad188
Smit Shah <smitshah681@gmail.com>
2025-04-22 17:34:08 -0400

    Created responsive headers

--- src/app/(consumer)/layout.tsx
+++ src/app/(consumer)/layout.tsx
@@ -10,6 +10,14 @@ import Link from "next/link";
 import { ReactNode, Suspense } from "react";
 import { Button } from "@/components/ui/button";
 import { getCurrentUser } from "@/services/clerk";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { AlignJustify } from "lucide-react";
 
 export default function ConsumerLayout({
   children,
@@ -25,7 +33,7 @@ export default function ConsumerLayout({
 function Navbar() {
   return (
     <header className="flex items-center bg-background shadow z-10 h-14">
-      <nav className="flex gap-4 w-full">
+      <nav className="hidden md:flex gap-4 w-full">
         <Link
           href="/"
           className="mr-auto hover:cursor-pointer flex items-center pl-10"
@@ -76,6 +84,95 @@ function Navbar() {
           </SignedOut>
         </Suspense>
       </nav>
+      <nav className="md:hidden flex w-full items-center gap-4">
+        <Sheet>
+          <Link
+            href="/"
+            className="mr-auto hover:cursor-pointer flex items-center pl-3"
+          >
+            <Image
+              src="/LMS_logo.png"
+              alt="Logo"
+              width={230}
+              height={150}
+              className="min-w-[150]"
+            />
+          </Link>
+          <Suspense>
+            <SignedOut>
+              <Button className="self-center hover:cursor-pointer mr-3" asChild>
+                <SignInButton>Sign In</SignInButton>
+              </Button>
+            </SignedOut>
+          </Suspense>
+          <Suspense>
+            <SignedIn>
+              <div className="size-8 self-center min-w-[38px]">
+                <UserButton
+                  appearance={{
+                    elements: {
+                      userButtonAvatarBox: { width: "100%", height: "100%" },
+                    },
+                  }}
+                />
+              </div>
+            </SignedIn>
+          </Suspense>
+          <SignedIn>
+            <SheetTrigger asChild>
+              <Button className="mr-6 hover:cursor-pointer" variant="outline">
+                <AlignJustify />
+              </Button>
+            </SheetTrigger>
+          </SignedIn>
+          <SheetContent className="w-[200px]">
+            <SheetHeader>
+              <Suspense>
+                <SignedIn>
+                  <div className="h-10"></div>
+                  <AdminLink />
+                  <SheetClose asChild>
+                    <Link
+                      className="px-2 h-8 flex items-center border-y border-black"
+                      href="/courses"
+                    >
+                      My Courses
+                    </Link>
+                  </SheetClose>
+                  <SheetClose asChild>
+                    <Link
+                      className="flex items-center px-2 h-8 border-b border-black"
+                      href="/purchases"
+                    >
+                      Purchase History
+                    </Link>
+                  </SheetClose>
+                </SignedIn>
+              </Suspense>
+              <Suspense>
+                <SignedOut>
+                  <SheetClose asChild>
+                    <Button
+                      className="hover:cursor-pointer mt-7 w-[100px] self-center"
+                      asChild
+                    >
+                      <SignInButton>Sign In</SignInButton>
+                    </Button>
+                  </SheetClose>
+                  <SheetClose asChild>
+                    <Button
+                      className="hover:cursor-pointer w-[100px] self-center"
+                      asChild
+                    >
+                      <SignUpButton>Sign Up</SignUpButton>
+                    </Button>
+                  </SheetClose>
+                </SignedOut>
+              </Suspense>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </nav>
     </header>
   );
 }
@@ -85,7 +182,10 @@ async function AdminLink() {
   const user = await getCurrentUser();
   if (user.role === "admin")
     return (
-      <Link className="hover:bg-accent/10 flex items-center px-2" href="/admin">
+      <Link
+        className="hover:bg-accent/10 flex items-center px-2 h-8 md:h-auto"
+        href="/admin"
+      >
         Admin
       </Link>
     );
--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -3,6 +3,14 @@ import Image from "next/image";
 import Link from "next/link";
 import { ReactNode, Suspense } from "react";
 import { Badge } from "@/components/ui/badge";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { AlignJustify } from "lucide-react";
 
 export default function ConsumerLayout({
   children,
@@ -18,9 +26,15 @@ export default function ConsumerLayout({
 function Navbar() {
   return (
     <header className="flex items-center bg-background shadow z-10 h-14">
-      <nav className="flex gap-4 w-full">
+      <nav className="hidden md:flex gap-4 w-full">
         <Link href="/" className="hover:cursor-pointer flex items-center pl-10">
-          <Image src="/LMS_logo.png" alt="Logo" width={230} height={100} />
+          <Image
+            src="/LMS_logo.png"
+            alt="Logo"
+            width={230}
+            height={100}
+            className="min-w-[230]"
+          />
         </Link>
         <Badge className="mr-auto flex self-center h-6">Admin</Badge>
 
@@ -50,7 +64,7 @@ function Navbar() {
             >
               Sales
             </Link>
-            <div className="size-8 self-center mr-10">
+            <div className="size-8 self-center mr-10 min-w-[38px]">
               <UserButton
                 appearance={{
                   elements: {
@@ -61,6 +75,75 @@ function Navbar() {
             </div>
           </SignedIn>
         </Suspense>
+      </nav>
+      <nav className="md:hidden flex w-full gap-4 items-center">
+        <Sheet>
+          <Link
+            href="/"
+            className="hover:cursor-pointer flex items-center pl-3"
+          >
+            <Image
+              src="/LMS_logo.png"
+              alt="Logo"
+              width={230}
+              height={150}
+              className="min-w-[150]"
+            />
+          </Link>
+          <Badge className="mr-auto flex self-center h-6">Admin</Badge>
+
+          <div className="hidden sm:flex size-8 self-center min-w-[38px]">
+            <UserButton
+              appearance={{
+                elements: {
+                  userButtonAvatarBox: { width: "100%", height: "100%" },
+                },
+              }}
+            />
+          </div>
+          <SheetTrigger asChild>
+            <Button
+              className="mr-6 hover:cursor-pointer self-center"
+              variant="outline"
+            >
+              <AlignJustify />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-[200px]">
+            <SheetClose asChild>
+              <Link
+                className="px-2 h-8 flex items-center mt-10 border-b border-black"
+                href="/admin/"
+              >
+                Admin Dashboard
+              </Link>
+            </SheetClose>
+            <SheetClose asChild>
+              <Link
+                className="px-2 h-8 flex items-center border-b border-black"
+                href="/admin/courses"
+              >
+                Courses
+              </Link>
+            </SheetClose>
+            <SheetClose asChild>
+              <Link
+                className="px-2 h-8 flex items-center border-b border-black"
+                href="/admin/products"
+              >
+                Products
+              </Link>
+            </SheetClose>
+            <SheetClose asChild>
+              <Link
+                className="px-2 h-8 flex items-center border-b border-black"
+                href="/admin/sales"
+              >
+                Sales
+              </Link>
+            </SheetClose>
+          </SheetContent>
+        </Sheet>
       </nav>
     </header>
   );
